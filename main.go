559ca91af8895447d5fd111dab238e9bb639b822
Likit Sae Lee <likit.usm@gmail.com>
2016-12-11 17:07:39 +0800

    allow to mark todo as completed

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/likitsaelee/gotodo/todo"
 	"gopkg.in/gin-gonic/gin.v1"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -20,11 +21,24 @@ func main() {
 
 	r.POST("/todo/add", func(c *gin.Context) {
 		newID := todos[len(todos)-1].ID + 1
-		newTodo := todo.Todo{ID: newID, Description: c.PostForm("todo")}
+		newTodo := todo.Todo{ID: newID, Description: c.PostForm("todo"), Completed: false}
 		todos = append(todos, newTodo)
 		c.Redirect(http.StatusMovedPermanently, "/")
 	})
 
-	fmt.Printf("Running on port 8080...")
+	r.POST("/todo/complete", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.PostForm("id"))
+		if err == nil {
+			for i, todo := range todos {
+				if id == todo.ID {
+					todos[i].Completed = true
+				}
+			}
+		}
+
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	log.Println("Running on port 8080...")
 	r.Run()
 }
--- todo/todo.go
+++ todo/todo.go
@@ -4,13 +4,14 @@ package todo
 type Todo struct {
 	ID          int
 	Description string
+	Completed   bool
 }
 
 // GetMockData returns a slice of fake todos
 func GetMockData() []Todo {
 	return []Todo{
-		Todo{ID: 1, Description: "Go Buy Milk"},
-		Todo{ID: 2, Description: "Go Make Apps"},
-		Todo{ID: 3, Description: "Go Sleepy"},
+		Todo{ID: 1, Description: "Go Buy Milk", Completed: false},
+		Todo{ID: 2, Description: "Go Make Apps", Completed: false},
+		Todo{ID: 3, Description: "Go Sleepy", Completed: false},
 	}
 }
