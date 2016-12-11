13375baf69e3a198227fe29da9159b4a9693afc9
Likit Sae Lee <likit.usm@gmail.com>
2016-12-11 17:27:26 +0800

    allow to delete todo

--- main.go
+++ main.go
@@ -26,6 +26,20 @@ func main() {
 		c.Redirect(http.StatusMovedPermanently, "/")
 	})
 
+	r.POST("/todo/delete", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.PostForm("id"))
+		if err == nil {
+			for i, todo := range todos {
+				if id == todo.ID {
+					// delete the element
+					todos = append(todos[:i], todos[i+1:]...)
+				}
+			}
+		}
+
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
 	r.POST("/todo/complete", func(c *gin.Context) {
 		id, err := strconv.Atoi(c.PostForm("id"))
 		if err == nil {
