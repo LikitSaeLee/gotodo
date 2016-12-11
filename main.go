53e64c8c420091a050dde18fad451dc3dd09d685
Likit Sae Lee <likit.usm@gmail.com>
2016-12-11 16:29:05 +0800

    enable create todo

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"github.com/likitsaelee/gotodo/todo"
 	"gopkg.in/gin-gonic/gin.v1"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*.html")
+	todos := todo.GetMockData()
+
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World")
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Todos": todos,
+		})
 	})
+
+	r.POST("/todo/add", func(c *gin.Context) {
+		newID := todos[len(todos)-1].ID + 1
+		newTodo := todo.Todo{ID: newID, Description: c.PostForm("todo")}
+		todos = append(todos, newTodo)
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	fmt.Printf("Running on port 8080...")
 	r.Run()
 }
